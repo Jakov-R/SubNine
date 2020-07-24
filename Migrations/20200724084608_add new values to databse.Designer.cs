6d681eed890438103d4e2ddb53726f62dc150a03
Jakov Raguž <Jakov.Raguz@fer.hr>
2020-07-24 13:00:56 +0200

    add migrations

--- Migrations/20200724084608_add new values to databse.Designer.cs
+++ Migrations/20200724084608_add new values to databse.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SubNineAPI.Entities;
 
 namespace SubNineAPI.Migrations
 {
     [DbContext(typeof(SubNineContext))]
-    partial class SubNineContextModelSnapshot : ModelSnapshot
+    [Migration("20200724084608_add new values to databse")]
+    partial class addnewvaluestodatabse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200724084608_add new values to databse.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SubNineAPI.Migrations
+{
+    public partial class addnewvaluestodatabse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "running" },
+                    { 2L, "jumping" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "Hrvatska" },
+                    { 2L, "Bosna i Hercegovina" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "100m" },
+                    { 2L, "long jump" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "Preliminary round" },
+                    { 2L, "Qualifications" },
+                    { 3L, "Heats" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Participations",
+                columns: new[] { "Id", "Result" },
+                values: new object[,]
+                {
+                    { 1L, 10.970000000000001 },
+                    { 2L, 22.219999999999999 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RangLists",
+                columns: new[] { "Id", "Place" },
+                values: new object[,]
+                {
+                    { 1L, 4 },
+                    { 2L, 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Participations",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Participations",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "RangLists",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "RangLists",
+                keyColumn: "Id",
+                keyValue: 2L);
+        }
+    }
+}
